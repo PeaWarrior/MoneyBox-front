cf35a00a289c2319945907af2a658ec87bfbc907
PeaWarrior <jacksonchen171@gmail.com>
2020-09-26 12:10:15 -0400

    added increment and decrement page for newslistings

--- src/components/stock/StockNewsListings.js
+++ src/components/stock/StockNewsListings.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Row, Col, Container, Button } from 'react-bootstrap';
 import StockNews from './StockNews';
 
 export default function StockNewsListings({ news }) {
+    const [currentPage, setCurrentPage] = useState(1);
 
+    const handleClickIncrementPage = event => {
+        setCurrentPage(currentPage+1)
+    };
+    const handleClickDecrementPage = event => {
+        setCurrentPage(currentPage-1)
+    };
+
+    console.log(currentPage);
     const renderArticles = () => {
-        return news.map(article => <StockNews key={article.id} {...article} />)
-    }
+        const currentNews = news.slice(((currentPage-1)*4), (currentPage*4))
+        return currentNews.map(article => <StockNews key={article.id} {...article} />)
+    };
 
     return (
         <Card.Body>
             {news.length > 0 ?
             <>
-                <h5>Recent News</h5>
+                <Container>
+
+                <Row>
+                    <Col>
+                        <h5>Recent News</h5>
+                    </Col>
+                    <Button 
+                        variant="link"
+                        disabled={currentPage === 1 ? true : false}
+                        onClick={handleClickDecrementPage}
+                    >
+                        {<i className="fas fa-chevron-left"></i>}
+                    </Button>
+                    <Button 
+                        variant="link"
+                        disabled={news.length <= currentPage * 4 ? true : false}
+                        onClick={handleClickIncrementPage}
+                        >
+                        <i className="fas fa-chevron-right"></i>
+                    </Button>
+                </Row>
+                </Container>
                 {renderArticles()}
             </>
             :
